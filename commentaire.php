d143791a680c3062c905b12c9829a99ecf5d0306
tony-guillot <tony.guilot@laplateforme.io>
2021-11-26 19:58:30 +0100

    livre d'or

--- commentaire.php
+++ commentaire.php
@@ -1,77 +1,70 @@
-<?php 
+<?php
 session_start();
 include('bdd.php');
 
+try{ 
+if(isset($_SESSION['id'])){
+    $getid = $_SESSION['id'];
+    $usercom = $bdd->prepare('SELECT * FROM utilisateurs WHERE id = ?');
+    $usercom->execute(array($getid));
+    $usercom = $usercom->fetch();
 
-            if(isset($_POST['valider'])){
+    if(isset($_POST['submit_commentaire'])){
 
-                if(!empty($_POST['commentaire'])){
+        if(isset($_SESSION['id'],$_POST['commentaire']) AND !empty($_POST['commentaire'])){
 
-                    $commentaire = nl2br(htmlspecialchars($_POST['commentaire']));
-                    $comm_date = date('d/m/Y');
-                    $id_user= $_SESSION['id'];
-                    $comm_user = $_SESSION['login'];
+            $commentaire = htmlspecialchars(($_POST['commentaire']));
+            if($_SESSION['id']) {
 
-                    $insert = $bdd->prepare('INSERT INTO commentaires (commentaire, id_utilisateur, date)VALUES(?,?;?)');
-                    $insert->execute(array($commentaire, $comm_user, $id_user));
-                   
+	            $ins = $bdd->prepare('INSERT INTO commentaires (commentaire, id_utilisateur, date) VALUES (?,?,NOW())');
+	            $ins->execute(array($commentaire,$getid));
+	            
+                $mgs= "Votre commentaire a bien été posté";
+	         }
+	         
+	      } else {
+	         $mgs = "Tous les champs doivent être complétés";
+	      
+        }
+    }
+}
+}
+catch(PDOException $e){
+    
+    echo 'echec : ' .$e->getMessage();
+}
 
-                    $valid = 'le commentaire à bien été envoyé';
-                }else{
-
-                    $erreur  = 'veuillez completer tout les champs';
-                }
-            }
-$id= $_GET['id'];
-
-$user = $bdd->prepare('SELECT * FROM utilisateurs WHERE id=?');
-$user->fetch();
-
-
-           
-
-
-
-
-
-
-
-
-
-
-
+$commentaires = $bdd->prepare('SELECT * FROM commentaires WHERE commentaire = ? ORDER BY id DESC');
+	   $commentaires->execute(array($getid));
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-     
 
-     <form action="" method="post">
-            
-     <h1>Poster un commentaire</h1> 
+<form class="form2" method="POST">
+    <div class="commentaire">
+    <h2 id="h2com">Commentaires:</h2>
 
-            <?php if(isset($erreur)){
-            
-            echo '<p classe erreur>' . $erreur . '</p>';
-            
-            }else(isset($valid)){
+    <?php if(isset($msg)){
+        
+        echo $msg;
 
-                echo '<p classe='erreur'>' . $valid . '</p>';
-            }
-            ?>
-            <input type="text" name="login" placeholder="Nom D'utilisateur">
-            <textarea name="commentaire"  cols="30" rows="10" placeholder="Votre commentaire"></textarea>
-            <input type="submit" name='valider' value="Poster votre commentaire">
-            <input type="hidden" name="id_utilisateur">
-     </form>
-</body>            
-</html>
+    }
+
+    ?>
+        <textarea id="textcom" name="commentaire" placeholder="Votre commentaire..."></textarea><br />
+        <input type="submit" value="Poster mon commentaire" name="submit_commentaire" />
+    </div>
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- livre-or.php
+++ livre-or.php
@@ -3,7 +3,49 @@ session_start();
 include('bdd.php');
 
 
+try{
+
+                                                    
+$allcomment = $bdd->query('SELECT * FROM utilisateurs INNER JOIN commentaires ON utilisateurs.id = commentaires.id');
+
+while($comment = $allcomment->fetch()){
+
+    $comment['commentaire'];
+}
+
+
+
 ?>
 
+
+
+
+
+
+
+
+<?php
+
+// while($comm = $allcomment->fetch()){
+//     $comm['id_utilisateur'] == $comm['login'];
+
+//     echo $comm['login'] . ':'.'  ' .$comm['commentaire']. '    '. $comm['date'];
+//     echo 'ok';
+//     ?>
+
+
+  
+    <?php
+
+
+
+} catch (PDOException $e) {
+
+    echo 'echec : ' . $e->getMessage();
+}
+
+
+?>
 <h2> <a href="commentaire">Poster un commentaire </a></h2>
+
 
